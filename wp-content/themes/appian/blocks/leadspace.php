4f3cbedc34a0132a4cb349c5a16e54b1414128b0
Surja11 <stha.surja123@gmail.com>
2026-05-22 17:29:18 +0545

    resolving multi dev error

--- wp-content/themes/appian/blocks/leadspace.php
+++ wp-content/themes/appian/blocks/leadspace.php
@@ -2,6 +2,6 @@
     $content = get_field('leadspace');
     $title   = $content['title'] ?? false;
     if(isset($title)):?>
-        <h1 class="leadspace"><?php echo esc_html($title);?></h1>
+        <h1 class="leadspace bg-primary"><?php echo esc_html($title);?></h1>
 <?php
     endif;    
