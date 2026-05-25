10f0876af0f5062ee2be5f7b95a2e94a20081d0a
Surja11 <stha.surja123@gmail.com>
2026-05-25 12:05:27 +0545

    AB-13: added block with html for text with CTA

---
+++ wp-content/themes/appian/blocks/our-story.php
@@ -0,0 +1,18 @@
+<section class="cta-content">
+    <span class="subheading">
+        Shaping the Future of Construction
+    </span>
+    <p class="cta-content__text">
+        founded in 1922, Heffron started as a simple plumbing and heating business. Now, it's a leading, family-owned innovator in construction. In the 21st century, oru deep expertise and unwavering dedication keep pushing the industry forward.
+
+    </p>
+
+    <button class="btn btn-primary">
+        <span>Our Story</span>
+       <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 8.41406H15M8 15.4141L15 8.41406L8 1.41406" stroke="#ffffff" stroke-width="2" stroke-miterlimit="5.75877" stroke-linecap="square"/>
+</svg>
+
+
+    </button>
+</section>
