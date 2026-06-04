f1ad4cd7c501dcad951d2f112a8b661655dd28ea
Surja11 <stha.surja123@gmail.com>
2026-06-04 09:36:09 +0545

    AB-27: made eclipses in leadspace

--- wp-content/themes/appian/blocks/leadspace.php
+++ wp-content/themes/appian/blocks/leadspace.php
@@ -1,12 +1,23 @@
-<?php
-    $content = get_field('leadspace');
-    $title   = $content['title'] ?? false;
-    if(isset($title)):?>
-        <h1 class="leadspace bg-primary"><?php echo esc_html($title);?></h1>
-<?php
-    endif;    ?>
-    
-    <section>
-    
-    <h1>Thisis leadspace</h1>
-    </section>
+
+<section class="home-leadspace w-100 overflow-hidden">
+
+<div class="home-leadspace__outer-eclipse">
+
+
+    <div class="home-leadspace__inner-eclipse">
+        <video autoplay muted loop playsinline class="home-leadspace__video" >
+        <source src="<?php echo get_template_directory_uri()?>/resources/images/Heffron.mp4" type="video/mp4">
+  
+        <!-- Fallback text for older browsers -->
+        Your browser does not support the HTML video tag.
+        </video>
+
+    </div>
+    <div class="home-leadspace__scroll-eclipse">
+
+    </div>
+
+
+</div>
+
+</section>
