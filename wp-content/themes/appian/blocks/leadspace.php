2df30f00486ccc65fda5062a98c341f788e2bfbd
Surja11 <stha.surja123@gmail.com>
2026-06-05 16:29:51 +0545

    AB-47: Added leadspace with animation on scroll

--- wp-content/themes/appian/blocks/leadspace.php
+++ wp-content/themes/appian/blocks/leadspace.php
@@ -1,23 +1,41 @@
-
 <section class="home-leadspace w-100 overflow-hidden">
 
-<div class="home-leadspace__outer-eclipse">
+<!-- outer ellipse -->
+    <div class="home-leadspace__outer-ellipse">
+
+    <!-- the scrolling arc -->
+        <span class="home-leadspace__scroll-ellipse"></span>
+
+        <!-- inner ellipse -->
+        <div class="home-leadspace__inner-ellipse">
+            <video autoplay muted loop playsinline class="home-leadspace__video">
+                <source src="<?php echo get_template_directory_uri() ?>/resources/images/Heffron.mp4" type="video/mp4">
+                Your browser does not support the HTML video tag.
+            </video>
+
+            <!-- overlay -->
+            <div class="home-leadspace__overlay position-absolute inset-0"></div>
 
 
-    <div class="home-leadspace__inner-eclipse">
-        <video autoplay muted loop playsinline class="home-leadspace__video" >
-        <source src="<?php echo get_template_directory_uri()?>/resources/images/Heffron.mp4" type="video/mp4">
-  
-        <!-- Fallback text for older browsers -->
-        Your browser does not support the HTML video tag.
-        </video>
+            <!-- text content -->
+            <div class="home-leadspace__text-content d-flex flex-column position-absolute">
+                <div class="home-leadspace__eyebrow">
+                    <span class="body body-small-all text-capitalize">
+                        Excellence Built Through Years of Trust
+                    </span>
+                </div>
+                <div class="home-leadspace__text-body">
+                    <h1 class="display-1 text-capitalize display-1--mobile-view">
+                        A history of excellence.
+                    </h1>
+                    <h1 class="h3 m-0">
+                        A history of excellence built on dedication, innovation, and consistently delivering outstanding results.
+                    </h1>
+                </div>
 
+            </div>
+        </div>
     </div>
-    <div class="home-leadspace__scroll-eclipse">
-
     </div>
-
-
-</div>
 
 </section>
--- wp-content/themes/appian/resources/scripts/modules/leadspace.js
+++ wp-content/themes/appian/resources/scripts/modules/leadspace.js
@@ -1,11 +1,24 @@
-class Leadspace {
-    constructor() {
-        this.events();
-    }
 
-    events() {
-       console.log('leadspace')
-    }
-}
 
-new Leadspace();
+const scrollElipse = document.querySelector('.home-leadspace__scroll-ellipse');
+const outerEllipse = document.querySelector('.home-leadspace__outer-ellipse');
+
+// listening for scroll event listener
+window.addEventListener('scroll', () => {
+
+    // getting the pixels scrolled by user
+    const scrolled = window.scrollY;
+    // console.log(scrolled)
+
+
+    // defining the total scroll distance needed to complete one full rotation
+    const total = window.innerHeight * 2;
+
+    // calculating scroll progress which is between 0 and 1
+    const progress = Math.min(scrolled / total, 1);
+
+    // calculating rotatiion degree
+    const rotation = -progress * 360;
+
+    scrollElipse.style.transform = `rotate(${rotation}deg)`;
+});
