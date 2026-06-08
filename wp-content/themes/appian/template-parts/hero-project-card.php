6df01be6c9f452025ddc573cb19e34bfb705e342
Surja11 <stha.surja123@gmail.com>
2026-06-08 12:39:52 +0545

    AB-17-BE: Added repeaters and looped through it to show cards

--- wp-content/themes/appian/template-parts/hero-project-card.php
+++ wp-content/themes/appian/template-parts/hero-project-card.php
@@ -5,16 +5,18 @@
  */
 
 $card           = get_query_var('card', []);
-$category       = $card['category'] ?? 'Renovation & Remodeling';
+$category       = $card['project_category'] ?? '';
 $category_short = $card['category_short'] ?? explode(' ', $category)[0];
-$title          = $card['title'] ?? 'Modern Building Solutions';
-$subtitle       = $card['subtitle'] ?? '';
+$title          = $card['project_title'] ?? '';
+$subtitle       = $card['project_subtitle'] ?? '';
+$card_image     = $card['project_image'] ?? null;
+$card_link      = $card['page_link'] ?? '#';
 ?>
 
 <article class="hero-project-card">
-    <a class="hero-project-card__link" href="#">
+    <a class="hero-project-card__link" href="<?php echo esc_url($card_link); ?>">
 
-        <div class="hero-project-card__image-wrap"></div>
+        <div class="hero-project-card__image-wrap" style="background-image: url('<?php echo esc_url($card_image['url']); ?>');"></div>
         <div class="hero-project-card__overlay"></div>
         <div class="hero-project-card__hover-overlay"></div>
 
