e53657d820dac158a34418bb14b59b13092d06b8
Surja11 <stha.surja123@gmail.com>
2026-06-09 00:54:21 +0545

    AB-37:Made frontend of our partners

--- wp-content/themes/appian/blocks/our-partner.php
+++ wp-content/themes/appian/blocks/our-partner.php
@@ -1,6 +1,6 @@
-<section class="our-partner" aria-labelledby="Our Partners Section">
+<section class="our-partner pt-12 pt-xl-20 " aria-labelledby="Our Partners Section">
 
-    <div class="our-partner__heading d-flex flex-column justify-content-center align-items-center">
+    <div class="our-partner__heading d-flex flex-column justify-content-center align-items-center ">
         <h2 class="mb-4">Our partner</h2>
         <picture>
             <source media="(min-width: 1200px)" srcset="<?php echo get_template_directory_uri(); ?>/resources/images/divider.svg">
@@ -8,110 +8,108 @@
         </picture>
     </div>
 
-    <!-- Top decorative border row -->
-    <!-- <div class="grid-block" aria-hidden="true"></div> -->
+   
 
     <div class="our-partner__partner-collection">
         <div class="row g-0"">
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/astra.png" alt="AstraZeneca">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3  ">
+            <div class="our-partner__single-partner  col-6 col-xl-3  ">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/wings.png" alt="State Department">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/unitedthera.png" alt="United Therapeutics">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/medstar.png" alt="MedStar Health">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/nih.png" alt="NIH">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/charles.png" alt="Charles River">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/regen.png" alt="RegenXBio">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/602.png" alt="602 Steamfitters">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/bird.png" alt="Plumbers & Steamfitters">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/catalent.png" alt="Catalent">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/biontech.png" alt="BioNTech">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/macrogenics.png" alt="MacroGenics">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/clesi.png" alt="Cel-Sci">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/instrument.png" alt="Plumbers DC">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner col col-6 col-xl-3">
+            <div class="our-partner__single-partner  col-6 col-xl-3">
                 <div class="our-partner__img-container">
                     <img src="<?php echo get_template_directory_uri();?>/resources/images/wvu.png" alt="WVU Medicine">
                 </div>
             </div>
 
-            <div class="our-partner__single-partner our-partner__single-partner--cta col col-6 col-xl-3">
-                <a href="#" class="text-decoration-none">View All Partners</  a>
+            <div class="our-partner__single-partner our-partner__single-partner--cta  col-6 col-xl-3">
+                <button class="btn btn-tertiary">View All Partners</button>
             </div>
 
         </div>
     </div>
 
-    <!-- Bottom decorative border row -->
-    <!-- <div class="grid-block" aria-hidden="true"></div> -->
+   
 
 </section>
