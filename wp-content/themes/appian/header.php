b2744319ec6ce5e22a607f6d7532d69235661ed6
sandhyashiwakoti <158501166+sandhyashiwakoti@users.noreply.github.com>
2026-06-10 02:16:49 +0545

    fix: resolve layout shift issue

--- wp-content/themes/appian/header.php
+++ wp-content/themes/appian/header.php
@@ -16,7 +16,16 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<style>
+		main {
+			padding-top: 72px !important;
+		}
+		@media (min-width: 1291px) {
+			main {
+				padding-top: 74px !important;
+			}
+		}
+	</style>
 	<?php wp_head(); ?>
 </head>
 
