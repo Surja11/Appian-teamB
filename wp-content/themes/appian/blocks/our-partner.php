aea0e264dfa951cb51454cd5b1ead28b52713801
Surja11 <stha.surja123@gmail.com>
2026-06-10 17:35:31 +0545

    AB-20: Corrected our partner module

--- wp-content/themes/appian/blocks/our-partner.php
+++ wp-content/themes/appian/blocks/our-partner.php
@@ -1,59 +1,54 @@
 <?php
-$partners = get_field( 'partners' );
+$partners = get_field('partners');
 
-$valid_partners = array();
-if ( ! empty( $partners ) && is_array( $partners ) ) {
-    foreach ( $partners as $row ) {
-        if ( ! empty( $row['partner_logo'] ) ) {
+$valid_partners = [];
+if (!empty($partners) && is_array($partners)) {
+    foreach ($partners as $row) {
+        if (!empty($row['partner_logo'])) {
             $valid_partners[] = $row;
         }
     }
 }
 
-if ( ! empty( $valid_partners ) ) :
+
+
+if (!empty($valid_partners)) :
 ?>
+<section class="our-partner-wrapper">
+<section class="our-partner" aria-labelledby="our-partners-heading">
 
-<section class="our-partner pt-12 pt-xl-20 " aria-labelledby="Our Partners Section">
-
-    <div class="our-partner__heading d-flex flex-column justify-content-center align-items-center ">
-        <h2 class="mb-4">Our partner</h2>
+    <div class="our-partner__heading">
+        <h2 id="our-partners-heading" class="mb-4">Our partner</h2>
         <picture>
             <source media="(min-width: 1200px)" srcset="<?php echo get_template_directory_uri(); ?>/resources/images/divider.svg">
-            <img src="<?php echo get_template_directory_uri(); ?>/resources/images/divider-small.svg" alt="divider line">
+            <img src="<?php echo get_template_directory_uri(); ?>/resources/images/divider-small.svg" alt="">
         </picture>
     </div>
 
-   
+    <div class="our-partner__grid-wrapper">
+        <div class="our-partner__partner-grid">
 
-    <div class="our-partner__partner-collection">
-        <div class="row g-0"">
-
-            <?php foreach ( $valid_partners as $partner ) :
-                $logo = $partner['partner_logo']; 
+            <?php foreach ($valid_partners as $partner) :
+                $logo = $partner['partner_logo'];
             ?>
-
-            <div class="our-partner__single-partner  col-6 col-xl-3">
+            <div class="our-partner__single-partner">
                 <div class="our-partner__img-container">
                     <img
-                        src="<?php echo esc_url( $logo['url'] ); ?>"
-                        alt="<?php echo esc_attr( $logo['alt'] ); ?>"
-                        <?php if ( ! empty( $logo['width'] ) ) : ?>width="<?php echo esc_attr( $logo['width'] ); ?>"<?php endif; ?>
-                        <?php if ( ! empty( $logo['height'] ) ) : ?>height="<?php echo esc_attr( $logo['height'] ); ?>"<?php endif; ?>
+                        src="<?php echo esc_url($logo['url']); ?>"
+                        alt="<?php echo esc_attr($logo['alt']); ?>"
                     >
                 </div>
             </div>
-
             <?php endforeach; ?>
 
-            <div class="our-partner__single-partner our-partner__single-partner--cta  col-6 col-xl-3">
+            <div class="our-partner__single-partner our-partner__single-partner--cta">
                 <button class="btn btn-tertiary">View All Partners</button>
             </div>
 
         </div>
     </div>
 
-   
-
+</section>
 </section>
 
-<?php endif;?>
+<?php endif?>
