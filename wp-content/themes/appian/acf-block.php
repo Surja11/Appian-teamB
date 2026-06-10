ca67af04ed8e9393ffbcd4ff4417c89483d77719
Mispragya005 <pragyam070@gmail.com>
2026-06-10 12:52:39 +0545

    merge conficts solved

--- wp-content/themes/appian/acf-block.php
+++ wp-content/themes/appian/acf-block.php
@@ -13,6 +13,8 @@ add_action('acf/init', function () {
             'our-history' => 'Our History',
             'slider-testimonial' => 'Slider Testimonial',
             'our-partner'=>'Our Partner',
+            'hero-projects'      => 'Hero Projects',
+            ''
         ];
 
         foreach ($boilerplateModules as $key => $mModule) {
