9f1b6a0daf57b0e27d4ec39e0689ca7f44ab37a2
sandhyashiwakoti <158501166+sandhyashiwakoti@users.noreply.github.com>
2026-06-12 02:15:30 +0545

    fix: testimonial slide issue

--- wp-content/themes/appian/blocks/slider-testimonial.php
+++ wp-content/themes/appian/blocks/slider-testimonial.php
@@ -1,6 +1,4 @@
 <?php
-$logo_1       = get_field('testimonial_logo_1');
-$logo_2       = get_field('testimonial_logo_2');
 $testimonials = get_field('testimonials');
 
 if (empty($testimonials) || !is_array($testimonials)) return;
@@ -8,21 +6,22 @@ if (empty($testimonials) || !is_array($testimonials)) return;
 $total = count($testimonials);
 
 if (!function_exists('testimonial_render_logo')) :
-function testimonial_render_logo($image) {
+function testimonial_render_logo($image, $class = '') {
     if (empty($image)) return;
     $file_path = get_attached_file($image['ID']);
+    $extra_class = $class ? ' class="' . esc_attr($class) . '"' : '';
+    
     if ($file_path && file_exists($file_path) && pathinfo($file_path, PATHINFO_EXTENSION) === 'svg') {
-        echo file_get_contents($file_path);
+        echo '<div' . $extra_class . '>' . file_get_contents($file_path) . '</div>';
         return;
     }
-    echo '<img src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '">';
+    echo '<img src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '"' . $extra_class . '>';
 }
 endif;
 ?>
 
-<section class="testimonial-section">
+<section class="testimonial-section py-5">
 
-    <!-- MOBILE: shown only below lg -->
     <div class="testimonial-card d-lg-none position-relative">
         <div class="testimonial-line"></div>
 
@@ -30,6 +29,7 @@ endif;
             <div class="swiper-wrapper">
                 <?php foreach ($testimonials as $slide) : ?>
                 <div class="swiper-slide">
+
                     <div class="quote-icon-wrapper">
                         <?php include get_template_directory() . '/resources/images/icon-quote.svg'; ?>
                     </div>
@@ -53,45 +53,54 @@ endif;
         </div>
     </div>
 
-    <!-- DESKTOP: shown only at lg and above -->
-    <div class="container d-none d-lg-block">
-        <div class="row">
-
-            <div class="col-lg-6 testimonial-left">
-                <div class="testimonial-logo">
-                    <?php testimonial_render_logo($logo_1); ?>
-                    <?php testimonial_render_logo($logo_2); ?>
-                </div>
-                <div class="testimonial-line"></div>
-            </div>
-
-            <div class="col-lg-6 testimonial-right">
+    <div class="container d-none d-lg-block testimonial-desktop-container">
+        <div class="row justify-content-center position-relative">
+            <div class="col-xl-10 col-lg-11">
+                
                 <div class="swiper testimonial-swiper-desktop">
                     <div class="swiper-wrapper">
                         <?php foreach ($testimonials as $slide) : ?>
                         <div class="swiper-slide">
-                            <div class="quote-icon-wrapper">
-                                <?php include get_template_directory() . '/resources/images/icon-quote.svg'; ?>
-                            </div>
-                            <h5>"<?php echo wp_kses_post($slide['quote']); ?>"</h5>
-                            <div class="author-meta">
-                                <h4 class="caption-2"><?php echo esc_html($slide['author_name']); ?></h4>
-                                <p class="caption-2"><?php echo esc_html($slide['author_company']); ?></p>
+                            <div class="row m-0 w-100 align-items-start">
+                                
+                                <div class="col-lg-3 p-0 position-relative testimonial-left-block">
+                                    <div class="testimonial-logo-wrapper">
+                                        <?php 
+                                        if (!empty($slide['title_logo'])) { testimonial_render_logo($slide['title_logo'], 'logo-title'); }
+                                        if (!empty($slide['icon_logo'])) { testimonial_render_logo($slide['icon_logo'], 'logo-icon'); }
+                                        ?>
+                                    </div>
+                                    <div class="testimonial-slide-divider"></div>
+                                </div>
+
+                                <div class="col-lg-9 p-0 testimonial-right-block">
+                                    <div class="quote-icon-wrapper">
+                                        <?php include get_template_directory() . '/resources/images/icon-quote.svg'; ?>
+                                    </div>
+                                    <h5>"<?php echo wp_kses_post($slide['quote']); ?>"</h5>
+                                    <div class="author-meta">
+                                        <h4 class="caption-2"><?php echo esc_html($slide['author_name']); ?></h4>
+                                        <p class="caption-2"><?php echo esc_html($slide['author_company']); ?></p>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                         <?php endforeach; ?>
                     </div>
                 </div>
 
-                <div class="slider-progress d-flex">
-                    <span class="progress-current">01</span>
-                    <div class="progress-bar-wrapper">
-                        <div class="progress-bar-fill"></div>
+                <div class="slider-progress-wrapper">
+                    <div class="slider-progress d-flex">
+                        <span class="progress-current">01</span>
+                        <div class="progress-bar-wrapper">
+                            <div class="progress-bar-fill"></div>
+                        </div>
+                        <span class="progress-total"><?php echo str_pad($total, 2, '0', STR_PAD_LEFT); ?></span>
                     </div>
-                    <span class="progress-total"><?php echo str_pad($total, 2, '0', STR_PAD_LEFT); ?></span>
                 </div>
-            </div>
 
+            </div>
         </div>
     </div>
 
