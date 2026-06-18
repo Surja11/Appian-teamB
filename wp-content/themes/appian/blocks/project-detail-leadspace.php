68a0daee0e22ffb725973a90d29d6d3d827e9a86
Surja11 <stha.surja123@gmail.com>
2026-06-18 17:17:16 +0545

    AB-209:written html structure for leadspace

--- wp-content/themes/appian/blocks/project-detail-leadspace.php
+++ wp-content/themes/appian/blocks/project-detail-leadspace.php
@@ -1,5 +1,22 @@
 <section class="project-detail-leadspace">
-<div>
 
-</div>
+    <div class="project-detail-leadspace__text-container d-flex justify-content-center align-items-center">
+        <div class="project-detail-leadspace__text-content ms-auto me-auto">
+            <h1 class="subheading-3">Renovation</h1>
+            <h2>Modern Building Solutions</h2>
+            <p class="body body-large">
+                Modern building solutions combine innovative design, sustainable materials, and advanced technology to create efficient, durable, and eco-friendly structures. These approaches focus on optimizing energy use, reducing environmental impact, and improving construction speed while maintaining high quality and functionality.
+            </p>
+
+            <p class="body body-small">
+                Smart designs built efficiently
+            </p>
+        </div>
+    </div>
+
+    <div class="project-detail-leadspace__img-part">
+
+    </div>
+
+
 </section>
